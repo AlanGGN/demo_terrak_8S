5cfc523b671ce4fb1385a190c9cd5e7c90f19338
AlanGGN <79706651+AlanGGN@users.noreply.github.com>
2023-04-26 16:56:50 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "flexibleengine_obs_bucket" "admin_bucket" {
 
 terraform {
   cloud {
-    organization = "algue" #projectY
+    organization = "algue" #projectYz
 
     workspaces {
       name = "demo_terra_k8s"
